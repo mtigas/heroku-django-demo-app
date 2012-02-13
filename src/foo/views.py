9ea600ec0436fa0a30d59a9855d97d446366327c
Mike Tigas <mike@tig.as>
2012-02-13 00:25:27 -0800

    fix views

--- src/foo/views.py
+++ src/foo/views.py
@@ -6,7 +6,8 @@ from foo.models import UserUploadedPhoto
 
 @cache_page(60)
 def two(request):
-    txt = "<p><a href='/'>back to home</a></p>"
+    txt = "<p>(<b>This is a demo of running a basic Django site on Heroku:</b> <a href=\"http://mike.tig.as/blog/2012/02/13/deploying-django-on-heroku/\">See the tutorial post</a> or <a href=\"https://github.com/mtigas/heroku-django-demo-app\">the project source code.)</p>"
+    txt += "<p><a href='/'>back to home</a></p>"
     txt += "<p>time: %s (to test 60 second cache)</p>" % datetime.now()
     for p in UserUploadedPhoto.objects.all().order_by('-id')[:5]:
         txt += "<p>%s<br><img style=\"max-width:300px\" src=\"%s\"></p>" % (p.name, p.photo.url)
--- src/my_test_app/views.py
+++ src/my_test_app/views.py
@@ -6,7 +6,8 @@ import sys
 
 @cache_page(60)
 def home(request):
-    txt = "<p><a href='/2/'>some pictures</a></p>"
+    txt = "<p>(<b>This is a demo of running a basic Django site on Heroku:</b> <a href=\"http://mike.tig.as/blog/2012/02/13/deploying-django-on-heroku/\">See the tutorial post</a> or <a href=\"https://github.com/mtigas/heroku-django-demo-app\">the project source code.)</p>"
+    txt += "<p><a href='/2/'>ImageField test here</a></p>"
     txt += "<p>time: %s (to test 60 second cache)</p>" % datetime.now()
-    txt += "<pre>" + pformat(sys.path) + "</pre>"
+    txt += "<pre>sys.path:\n" + pformat(sys.path) + "</pre>"
     return HttpResponse(txt)
